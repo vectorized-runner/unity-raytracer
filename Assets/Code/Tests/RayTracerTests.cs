25948631b94b7dc4c2b410f8376e5a7241d5243d
Bartu Baş <bartubas99@gmail.com>
2022-11-14 13:06:40 +0300

    Fix tests

--- Assets/Code/Tests/RayTracerTests.cs
+++ Assets/Code/Tests/RayTracerTests.cs
@@ -19,6 +19,12 @@ namespace RayTracer
 				Center = new float3(300, 0, 0),
 				Radius = 1f
 			};
+
+			var roots = RMath.RaySphereIntersectionAnalytical(ray, sphere, out var p0, out var p1);
+			Assert.AreEqual(2, roots);
+
+			Assert.IsTrue(RMath.AreEqual(p0, new float3(299, 0, 0)), $"NotEqual: {p0}");
+			Assert.IsTrue(RMath.AreEqual(p1, new float3(301, 0, 0)), $"NotEqual: {p1}");
 		}
 	}
 }
