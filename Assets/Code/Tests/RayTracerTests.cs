be4140d8d984b5bb3b8436971d713463c857458f
Bartu Baş <bartubas99@gmail.com>
2022-11-14 13:06:40 +0300

    Ray tracer tests setup

---
+++ Assets/Code/Tests/RayTracerTests.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+public static class RayTracerTests
+{
+    [Test]
+    public static void Case1()
+    {
+    }
+    
+
+}
