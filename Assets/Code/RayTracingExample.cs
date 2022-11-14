7b9df0b74e9818a032fbc57b4f4bb45506a7a304
Bartu Baş <bartubas99@gmail.com>
2022-11-14 13:06:39 +0300

    Add: Ray-Sphere intersection code

--- Assets/Code/RayTracingExample.cs
+++ Assets/Code/RayTracingExample.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
 
@@ -8,6 +9,11 @@ public struct Ray
 {
 	public float3 Origin;
 	public float3 Direction;
+
+	public float3 GetPoint(float distance)
+	{
+		return Origin + Direction * distance;
+	}
 }
 
 [Serializable]
@@ -62,6 +68,12 @@ public struct ImagePlane
 	}
 }
 
+public struct Sphere
+{
+	public float3 Center;
+	public float Radius;
+}
+
 public struct CameraData
 {
 	public float3 Position;
@@ -78,9 +90,14 @@ public class RayTracingExample : MonoBehaviour
 
 	public bool ToggleDrawImagePlane = true;
 	public bool ToggleDrawRays = true;
+	private bool ToggleDrawIntersections = true;
+
 	public Color ImagePlaneColor = Color.red;
 	public Color RayColor = Color.yellow;
+	public Color IntersectionColor = Color.cyan;
 
+	private List<Sphere> Spheres;
+	
 	private void OnDrawGizmos()
 	{
 		if (!Application.isPlaying)
@@ -116,6 +133,14 @@ public class RayTracingExample : MonoBehaviour
 		{
 			DrawRays(CameraData);
 		}
+
+		if (ToggleDrawIntersections)
+		{
+			UpdateSpheresInScene();
+			DrawIntersections(CameraData);
+		}
+	}
+
 	private void UpdateSpheresInScene()
 	{
 		Spheres.Clear();
@@ -136,6 +161,98 @@ public class RayTracingExample : MonoBehaviour
 			}
 		}
 	}
+
+	private void DrawIntersections(CameraData cameraData)
+	{
+		var resX = ImagePlane.Resolution.X;
+		var resY = ImagePlane.Resolution.Y;
+		var topLeft = ImagePlane.GetRect(cameraData).TopLeft;
+		var up = cameraData.Up;
+		var right = cameraData.Right;
+		var horizontalLength = ImagePlane.HorizontalLength;
+		var verticalLength = ImagePlane.VerticalLength;
+		var cameraPosition = cameraData.Position;
+
+		for (int x = 0; x < resX; x++)
+		for (int y = 0; y < resY; y++)
+		{
+			// Get ray
+			var rightMove = (x + 0.5f) * horizontalLength / resX;
+			var downMove = (y + 0.5f) * verticalLength / resY;
+			var pointOnPlane = topLeft + rightMove * right - up * downMove;
+			var ray = new Ray
+			{
+				Origin = cameraPosition,
+				Direction = math.normalize(pointOnPlane - cameraPosition)
+			};
+			
+			// Check intersection against each sphere
+			
+			
+		}
+		
+		throw new NotImplementedException();
+	}
+
+	private static bool IsLengthEqual(float3 v, float length)
+	{
+		return math.abs(math.lengthsq(v) - length * length) < 0.001f;
+	}
+
+	// Derived from:
+	// https://www.scratchapixel.com/lessons/3d-basic-rendering/minimal-ray-tracer-rendering-simple-shapes/ray-sphere-intersection
+	private int RaySphereIntersection(Ray ray, Sphere sphere, out float3 intersectA, out float3 intersectB)
+	{
+		Debug.Assert(IsLengthEqual(ray.Direction, 1f));
+		
+		var oc = sphere.Center - ray.Origin;
+		var rSquared = sphere.Radius * sphere.Radius;
+		var a = 1f;
+		var b = 2 * math.dot(ray.Direction, oc);
+		var c = math.dot(oc, oc) - rSquared;
+
+		switch (SolveQuadraticEquation(a, b, c, out var x0, out var x1))
+		{
+			case 0:
+			{
+				intersectA = default;
+				intersectB = default;
+				return 0;
+			}
+			case 1:
+			{
+				intersectA = intersectB = ray.GetPoint(x0);
+				return 1;
+			}
+			case 2:
+			{
+				intersectA = ray.GetPoint(x0);
+				intersectB = ray.GetPoint(x1);
+				return 2;
+			}
+			default:
+				throw new ArgumentOutOfRangeException();
+		}
+	}
+
+	private int SolveQuadraticEquation(float a, float b, float c, out float x0, out float x1)
+	{
+		var discriminant = b * b - 4 * a * c;
+		if (discriminant < 0)
+		{
+			x0 = 0;
+			x1 = 0;
+			return 0;
+		}
+		if (discriminant == 0)
+		{
+			x0 = x1 = 0.5f * -b / a;
+			return 1;
+		}
+
+		x0 = 0.5f * (-b + discriminant) / a;
+		x1 = 0.5f * (-b - discriminant) / a;
+		return 2;
 	}
 
 	private void DrawRays(CameraData cameraData)
