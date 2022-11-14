15463b851667d6b22504cf88fe8c35d28fee8799
Bartu Baş <bartubas99@gmail.com>
2022-11-14 13:06:40 +0300

    Move data classes, get intersection code

---
+++ Assets/Code/Data/Triangle.cs
@@ -0,0 +1,11 @@
+using System;
+using Unity.Mathematics;
+
+namespace RayTracer
+{
+	[Serializable]
+	public struct Triangle
+	{
+		public float3 Vertices;
+	}
+}
--- Assets/Code/Math/RMath.cs
+++ Assets/Code/Math/RMath.cs
@@ -6,6 +6,58 @@ namespace RayTracer
 {
 	public static class RMath
 	{
+		// TODO-Port: Code taken from the internet, you know what to do.
+		public static bool RayTriangleIntersection(Ray ray, Triangle triangle, out float3 intersection)
+		{
+			const float epsilon = 0.0000001f;
+
+			var vertex0 = triangle.Vertices.x;
+			var vertex1 = triangle.Vertices.y;
+			var vertex2 = triangle.Vertices.z;
+			float3 edge1, edge2, h, s, q;
+			float a, f, u, v;
+			edge1 = vertex1 - vertex0;
+			edge2 = vertex2 - vertex0;
+			h = cross(ray.Direction, edge2);
+			a = dot(edge1, h);
+
+			if (a > -epsilon && a < epsilon)
+			{
+				intersection = default;
+				return false; // This ray is parallel to this triangle.
+			}
+
+			f = 1.0f / a;
+			s = ray.Origin - vertex0;
+			u = f * dot(s, h);
+			if (u < 0.0 || u > 1.0)
+			{
+				intersection = default;
+				return false;
+			}
+
+			q = cross(s, edge1);
+			v = f * dot(ray.Direction, q);
+			if (v < 0.0 || u + v > 1.0)
+			{
+				intersection = default;
+				return false;
+			}
+
+			// At this stage we can compute t to find out where the intersection point is on the line.
+			var t = f * dot(edge2, q);
+			if (t > epsilon) // ray intersection
+			{
+				intersection = ray.GetPoint(t);
+				return true;
+			}
+
+			// This means that there is a line intersection but not a ray intersection.
+			intersection = default;
+			return false;
+		}
+
+
 		// TODO-Port: Cleanup this code when porting, it uses code taken from internet
 		// TODO-Optimize: Skip Quadratic Equation part, use the most optimized math formula only
 		// TODO-Optimize: Store RadiusSquared on Spheres?
@@ -28,7 +80,7 @@ namespace RayTracer
 			// Ignore discriminant == 0 because it won't practically happen
 			var sqrtDiscriminant = sqrt(discriminant);
 			var bigRoot = -uoc + sqrtDiscriminant;
-			
+
 			if (bigRoot < 0)
 			{
 				closestIntersection = default;
