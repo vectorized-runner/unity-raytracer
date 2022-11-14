3a174f356407081b3d26bd26eff089eb2467cad4
Bartu Baş <bartubas99@gmail.com>
2022-11-14 13:06:40 +0300

    Add: Triangle normal and center

--- Assets/Code/Data/Triangle.cs
+++ Assets/Code/Data/Triangle.cs
@@ -9,5 +9,16 @@ namespace RayTracer
 		public float3 Vertex0;
 		public float3 Vertex1;
 		public float3 Vertex2;
+
+		public float3 Normal
+		{
+			get
+			{
+				var v = math.cross(Vertex2 - Vertex0, Vertex1 - Vertex0);
+				return v / math.length(v);
+			}
+		}
+
+		public float3 Center => (Vertex0 + Vertex1 + Vertex2) / 3;
 	}
 }
