2c2e1450d624495387d3034b28ed1cd6876e2879
Bartu Baş <bartubas99@gmail.com>
2022-11-14 13:06:41 +0300

    Rgb operators and simplicity

--- Assets/Code/Data/Rgb.cs
+++ Assets/Code/Data/Rgb.cs
@@ -1,5 +1,6 @@
 using System;
 using Unity.Mathematics;
+using UnityEngine;
 
 namespace RayTracer
 {
@@ -7,5 +8,17 @@ namespace RayTracer
 	public struct Rgb
 	{
 		public float3 Value;
+
+		public Color Color => new Color(Value.x, Value.y, Value.z, 1f);
+
+		public Rgb(float3 value)
+		{
+			Value = value;
+		}
+
+		public static Rgb operator +(Rgb a) => a;
+		public static Rgb operator -(Rgb a) => new(-a.Value);
+		public static Rgb operator +(Rgb a, Rgb b) => new(a.Value + b.Value);
+		public static Rgb operator -(Rgb a, Rgb b) => a + -b;
 	}
 }
